6ec1af3b0694014b1466a0f560c8bd981bbf18b1
Saimun <saimun4u@gmail.com>
2021-07-12 17:11:56 -0500

    js toggle-switch changes icon, navbar and textbox to dark theme

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const toggleBtn = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
 
 //Toggle theme function
 
@@ -6,9 +13,21 @@ function switchTheme(event){
     // console.log(event.target.checked)
     if (event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     }else{
-        document.documentElement.setAttribute('data-theme', 'light')
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
+}
+
+//Dark Mode function
+
+function darkMode(){
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
 }
 
 //Event Listener
