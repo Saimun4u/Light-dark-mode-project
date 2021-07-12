8ce0f1b9f4451f9cde4f2e3f20d58cf790194be8
Saimun <saimun4u@gmail.com>
2021-07-12 17:24:06 -0500

    js toggle button with functionality styling for light mode added

--- script.js
+++ script.js
@@ -33,6 +33,19 @@ function darkMode(){
     image3.src = '../img/undraw_conceptual_idea_dark.svg';
 }
 
+//Light Mode function
+
+function lightMode(){
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Lighth Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    image1.src = './img/undraw_proud_coder_light.svg';
+    image2.src = './img/undraw_feeling_proud_light.svg';
+    image3.src = '../img/undraw_conceptual_idea_light.svg';
+}
+
 //Event Listener
 
 toggleBtn.addEventListener('change', switchTheme);
